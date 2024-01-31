b4d6d3414abc436e37746e9c00baf4b4c04a8bcf
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2024-01-31 09:33:15 +0530

    omit

--- Quizie/src/App.tsx
+++ Quizie/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+// import Home from "./components/Home.tsx";
+// import Home from "./components/Home";
+import Home from "./components/Home";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 };
 
 export default App;
