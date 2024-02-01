e33e225a1ad60bd4180b283f7665dcef5c2a40a4
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2024-02-01 15:18:37 +0530

    structured

--- Quizie/src/App.tsx
+++ Quizie/src/App.tsx
@@ -1,14 +1,53 @@
-import React from "react";
-// import Home from "./components/Home.tsx";
-// import Home from "./components/Home";
-import Home from "./components/Home";
+import { useEffect, useState } from "react";
+import Quiz from "./components/Quiz";
 
-const App = () => {
+interface Question {
+  question: string;
+  correct_answer: string;
+  incorrect_answer: string[];
+}
+
+function App() {
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+
+  const fetchQuestions = async () => {
+    try {
+      const url = "https://opentdb.com/api.php?amount=10";
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.results && data.results.length > 0) {
+        setQuestions(data.results);
+      } else {
+        console.log("No questions returned in the API response");
+      }
+    } catch (error) {
+      console.log("Error fetching questions: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
+  const handleAnswerSelected = (isCorrect: boolean) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+  };
+
+  const handleNextQuestion = () => {
+    setCurrentQuestionIndex((currentQuestionIndex + 1) % questions.length);
+  };
+
   return (
-    <div>
-      <Home />
+    <div className="app-container">
+      <h1>Quiz App</h1>
+      <Quiz data={questions} />
     </div>
   );
-};
+}
 
 export default App;
--- Quizie/src/components/Home.tsx
+++ Quizie/src/components/Home.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="home">
+      <h1>
+        {" "}
+        Lets Check your <span>IQ</span> 🧠 !!
+      </h1>
+      <button onClick={() => navigate("/quiz")} className="btn">
+        Quiz
+      </button>
+    </div>
+  );
 };
 
 export default Home;
