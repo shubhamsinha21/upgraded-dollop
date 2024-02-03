a8dae92ce745665e85d9cd6cffab8947a5a68891
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2024-02-03 14:37:25 +0530

    update

--- Quizie/src/App.tsx
+++ Quizie/src/App.tsx
@@ -1,53 +1,8 @@
-import { useEffect, useState } from "react";
-import Quiz from "./components/Quiz";
-
-interface Question {
-  question: string;
-  correct_answer: string;
-  incorrect_answer: string[];
-}
+import React from "react";
+import Navigation from "./routes/Navigation";
 
 function App() {
-  const [questions, setQuestions] = useState<Question[]>([]);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
-  const [score, setScore] = useState<number>(0);
-
-  const fetchQuestions = async () => {
-    try {
-      const url = "https://opentdb.com/api.php?amount=10";
-      const response = await fetch(url);
-      const data = await response.json();
-
-      if (data.results && data.results.length > 0) {
-        setQuestions(data.results);
-      } else {
-        console.log("No questions returned in the API response");
-      }
-    } catch (error) {
-      console.log("Error fetching questions: ", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchQuestions();
-  }, []);
-
-  const handleAnswerSelected = (isCorrect: boolean) => {
-    if (isCorrect) {
-      setScore(score + 1);
-    }
-  };
-
-  const handleNextQuestion = () => {
-    setCurrentQuestionIndex((currentQuestionIndex + 1) % questions.length);
-  };
-
-  return (
-    <div className="app-container">
-      <h1>Quiz App</h1>
-      <Quiz data={questions} />
-    </div>
-  );
+  return <Navigation />;
 }
 
 export default App;
